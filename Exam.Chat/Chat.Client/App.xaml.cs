ce943927c54de2574fd952ffec203977374243b8
sergeybiophys <roven2hati@gmail.com>
2021-11-17 19:00:11 +0200

    start creating the client side

--- Exam.Chat/Chat.Client/App.xaml.cs
+++ Exam.Chat/Chat.Client/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Chat.Client
+namespace Chat.Client.Views
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Exam.Chat/Chat.Client/Views/MainWindow.xaml.cs
+++ Exam.Chat/Chat.Client/Views/MainWindow.xaml.cs
@@ -13,7 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Chat.Client
+
+namespace Chat.Client.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
