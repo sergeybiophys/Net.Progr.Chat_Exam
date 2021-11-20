9db7f1880a024e67d42ff60f2294901d4e959e20
sergeybiophys <roven2hati@gmail.com>
2021-11-20 14:14:03 +0200

    added modelsLibrary

--- Exam.Chat/Chat.Client/Views/MainWindow.xaml.cs
+++ Exam.Chat/Chat.Client/Views/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 
-namespace Chat.Client.Views
+namespace ChatClient.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
---
+++ Exam.Chat/ChatModelsLibrary/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatModelsLibrary.Models
+{
+    public class Client
+    {
+        public int PORT { get; set; } = 55_555;
+        public string HOST { get; set; } = "127.0.0.1";
+        public Socket clientSocket { get; set; }
+        public string NickName { get; set; }
+    }
+}
