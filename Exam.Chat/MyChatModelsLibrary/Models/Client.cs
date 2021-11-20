910947e2d44a869e7f241d8dc3c18815971e88b8
sergeybiophys <roven2hati@gmail.com>
2021-11-20 15:43:33 +0200

    modified Client v2

--- Exam.Chat/MyChatModelsLibrary/Models/Client.cs
+++ Exam.Chat/MyChatModelsLibrary/Models/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,27 @@ namespace MyChatModelsLibrary.Models
 {
     public class Client
     {
-        public int PORT { get; set; } = 55_555;
-        public string HOST { get; set; } = "127.0.0.1";
+        const int PORT  = 55_555;
+        const string HOST  = "127.0.0.1";
         public Socket clientSocket { get; set; }
-        public string NickName { get; set; }
+        public string NickName { get; set; } = "Mr.Robot";
+        public IPEndPoint iPEndPoint { get; set; }
+
+        public Client() 
+        {
+            this.clientSocket = new Socket(AddressFamily.InterNetwork,
+                                                    SocketType.Stream,
+                                                    ProtocolType.Tcp);
+            this.iPEndPoint = new IPEndPoint(IPAddress.Parse(HOST), PORT);
+        }
+        public Client(string nickname)
+        {
+            this.NickName = nickname;
+
+            this.clientSocket = new Socket(AddressFamily.InterNetwork,
+                                        SocketType.Stream,
+                                        ProtocolType.Tcp);
+            this.iPEndPoint = new IPEndPoint(IPAddress.Parse(HOST), PORT);
+        }
     }
 }
