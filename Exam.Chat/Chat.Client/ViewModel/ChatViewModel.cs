5a3ab24d4d3434bfcdbb6eb4e6f2e5a8a3c472e9
sergeybiophys <roven2hati@gmail.com>
2021-11-20 14:28:21 +0200

    added viewModel&Infrastructure

---
+++ Exam.Chat/Chat.Client/ViewModel/ChatViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Chat.Client.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.ViewModel
+{
+    class ChatViewModel:Notifier
+    {
+    }
+}
