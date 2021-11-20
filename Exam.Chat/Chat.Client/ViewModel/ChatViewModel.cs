7e436bf5ca5dc2cf4cf7c757763bb5d8747a0c12
sergeybiophys <roven2hati@gmail.com>
2021-11-20 16:39:02 +0200

    work with ChatViewModel

--- Exam.Chat/Chat.Client/ViewModel/ChatViewModel.cs
+++ Exam.Chat/Chat.Client/ViewModel/ChatViewModel.cs
@@ -2,6 +2,7 @@
 using MyChatModelsLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -47,15 +48,15 @@ namespace ChatClient.ViewModel
             get => sendIsEnable;
         }
 
-        private Visibility usernameTakenLabelIsEnable;
-        public Visibility UsernameTakenLabelIsEnable
+        private Visibility nickNameTakenLabelIsEnable;
+        public Visibility NickNameTakenLabelIsEnable
         {
             set
             {
-                usernameTakenLabelIsEnable = value;
+                nickNameTakenLabelIsEnable = value;
                 Notify();
             }
-            get => usernameTakenLabelIsEnable;
+            get => nickNameTakenLabelIsEnable;
         }
 
         private string textMessage;
@@ -88,19 +89,127 @@ namespace ChatClient.ViewModel
         public ICommand SendEmailCommand { set; get; }
         #endregion
 
+        #region Data
+
+        private static Client client;
+        private string selectedNick;
+        private string nickName;
+
+        #endregion
+
+        public string NickName
+        {
+            set
+            {
+                nickName = value;
+                Notify();
+            }
+            get => nickName;
+        }
+        public string SelectedNick
+        {
+            set
+            {
+                selectedNick = value;
+                if (value == ClientsNick.ElementAt(0))
+                    Receiver = ClientsNick.ElementAt(0).ToLower();
+                else
+                    Receiver = $"only {value}";
+
+                Notify();
+            }
+            get => selectedNick;
+        }
+        public ObservableCollection<string> ClientsNick { get; set; }
+        public ObservableCollection<MessageUI> MessagessItems { set; get; }
+        public ChatViewModel()
+        {
+            client = new Client();
+            ClientsNick = new ObservableCollection<string>();
+            ClientsNick.Add("Everyone");
+            SelectedNick = ClientsNick.ElementAt(0);
+
+            ConnectIsVisible = Visibility.Visible;
+            WarningVisibility = NickNameTakenLabelIsEnable = Visibility.Hidden;
+            MessagessItems = new ObservableCollection<MessageUI>();
+            InitCommands();
+
+            Application.Current.MainWindow.Closing += new CancelEventHandler(ChatWindow_Closing);
+        }
 
 
+
+        private void InitCommands()
+        {
+            ConnectCommand = new RelayCommand(x => Task.Run(() =>
+            {
+               
+            }));
+
+            SendCommand = new RelayCommand(x => Task.Run(() =>
+            {
+               if(!Connect())
+                {
+                    NickNameTakenLabelIsEnable = Visibility.Visible;
+                }
+                ConnectIsVisible = NickNameTakenLabelIsEnable = Visibility.Hidden;
+                WarningVisibility = Visibility.Visible;
+                SendIsEnable = true;
+                client.NickName = NickName;
+
+                ReceiveMessage();
+            }
+            ));
+        }
+
+        public bool Connect()
+        {
+            return true;
+        }
+
+        static byte[] ReceiveMessage()
+        {
+            const int BUFF_SIZE = 255;
+            byte[] buff = new byte[BUFF_SIZE];
+
+            client.clientSocket.Receive(buff);
+
+            return buff;
+        }
 
         public void SendMessage(ClientMessage msg)
         {
+            
 
+        }
+        static byte[] ConvertStringToBytes(string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+
+        static string ConvertBytesToString(byte[] data)
+        {
+            return Encoding.UTF8.GetString(data, 0, data.Length);
+        }
+        static void StartListening()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    byte[] buff = ReceiveMessage();
+                    string message = ConvertBytesToString(buff);
+
+                    Console.WriteLine($"Receive message from the server - {message}");
+                }
+            });
         }
 
         void ChatWindow_Closing(object sender, CancelEventArgs e )
         {
             try
             {
-
+                
             }
             catch
             {
