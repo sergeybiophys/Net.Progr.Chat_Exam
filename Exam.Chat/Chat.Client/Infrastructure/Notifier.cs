d60779c8ffce27c433567d7cd16557bb6e0756e2
sergeybiophys <roven2hati@gmail.com>
2021-11-20 14:41:24 +0200

    modified Client

--- Exam.Chat/Chat.Client/Infrastructure/Notifier.cs
+++ Exam.Chat/Chat.Client/Infrastructure/Notifier.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chat.Client.Infrastructure
+namespace ChatClient.Infrastructure
 {
     class Notifier:INotifyPropertyChanged
     {
--- Exam.Chat/Chat.Client/ViewModel/ChatViewModel.cs
+++ Exam.Chat/Chat.Client/ViewModel/ChatViewModel.cs
@@ -1,13 +1,111 @@
-﻿using Chat.Client.Infrastructure;
+﻿using ChatClient.Infrastructure;
+
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ChatClient.ViewModel
 {
     class ChatViewModel:Notifier
     {
+        #region UI
+        private Visibility connectIsVisible;
+        public Visibility ConnectIsVisible
+        {
+            set
+            {
+                connectIsVisible = value;
+                Notify();
+            }
+            get => connectIsVisible;
+        }
+
+        private Visibility warningVisibility;
+        public Visibility WarningVisibility
+        {
+            set
+            {
+                warningVisibility = value;
+                Notify();
+            }
+            get => warningVisibility;
+        }
+
+        private bool sendIsEnable;
+        public bool SendIsEnable
+        {
+            set
+            {
+                sendIsEnable = value;
+                Notify();
+            }
+            get => sendIsEnable;
+        }
+
+        private Visibility usernameTakenLabelIsEnable;
+        public Visibility UsernameTakenLabelIsEnable
+        {
+            set
+            {
+                usernameTakenLabelIsEnable = value;
+                Notify();
+            }
+            get => usernameTakenLabelIsEnable;
+        }
+
+        private string textMessage;
+        public string TextMessage
+        {
+            set
+            {
+                textMessage = value;
+                Notify();
+            }
+            get => textMessage;
+        }
+
+        private string receiver;
+        public string Receiver
+        {
+            set
+            {
+                receiver = value;
+                Notify();
+            }
+            get => receiver;
+        }
+
+        #endregion
+
+        #region Commands
+        public ICommand ConnectCommand { set; get; }
+        public ICommand SendCommand { set; get; }
+        public ICommand SendEmailCommand { set; get; }
+        #endregion
+
+
+
+
+        public void SendMessage(ClientMessage msg)
+        {
+
+        }
+
+        void ChatWindow_Closing(object sender, CancelEventArgs e )
+        {
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
+        }
     }
 }
